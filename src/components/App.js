2291ced6010bfb1ab605a9168815b0bbd0ec7b05
vedansh kumar <56169665+vedanshk@users.noreply.github.com>
2023-04-05 21:33:41 +0530

    ref

---
+++ src/components/App.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function App () {
+  return (
+    <div> 
+    <a href='http://localhost:4001/auth/google'> Sign in google</a>
+   </div>
+  )
+}
+
+export default App;
